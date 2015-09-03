4c467c868eb555efcf065ff6479ecc7111745b38
ferron99 <nferro@terpmail.umd.edu>
2015-09-03 16:30:40 -0400

    Made modifications
    Created distinct motion for each ellipse

--- circles1.java
+++ circles1.java
@@ -1,23 +1,28 @@
-//// Wild circles #1.
+/// Wild circles #1.
 
 // GLOBAL VARIABLES //
-float x,y;
+float x,y,a,b,c,d;
 
 // Setup:  screen size, initialization. //
 void setup() {
   size(1600, 900);        // Try changing the width & height.
   x=  width/2;
   y=  height/2;
+  a= width/2;
+  b= height/2;
+  c= width/2;
+  d= height/2;
 }
 
 void draw() {
-  ellipse(x, y, 80, 80);
+  ellipse(a, b, 80, 80);
   ellipse(x+60, y+60, 40, 40);
-  ellipse(x+90, y+90, 20, 20);
+  ellipse(c+90, d+90, 20, 20);
  
   // Move them???? //--              
   x=  x + 2;  y=  y + 1;
+  a=  a + 1;  b=  b + 2;
+  c=  c + 3;  d=  d + .5;
   // Insert BREAK here ^
   // to uncomment the above code,
 }
-
