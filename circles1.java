fac41efa512675c91c3b2dbd236bf62112d9fafe
ferron99 <nferro@terpmail.umd.edu>
2015-09-03 16:25:39 -0400

    Made modifications
    Changed window size and created motion.

--- circles1.java
+++ circles1.java
@@ -5,18 +5,19 @@ float x,y;
 
 // Setup:  screen size, initialization. //
 void setup() {
-  size(500, 500);        // Try changing the width & height.
+  size(1600, 900);        // Try changing the width & height.
   x=  width/2;
   y=  height/2;
 }
 
-// Next frame:  draw three circles. //
 void draw() {
   ellipse(x, y, 80, 80);
   ellipse(x+60, y+60, 40, 40);
   ellipse(x+90, y+90, 20, 20);
  
-  // Move them???? //--              x=  x + 2;  y=  y + 1;
+  // Move them???? //--              
+  x=  x + 2;  y=  y + 1;
   // Insert BREAK here ^
   // to uncomment the above code,
 }
+
